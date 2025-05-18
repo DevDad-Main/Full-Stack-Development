b17c964557a23519eb595faab2754b8d5b62447e
Oliver <softwaredevdad@gmail.com>
2025-05-18 23:12:22 +0200

    New Website and updates to course work

--- Web-Development/JQUERY/index.js
+++ Web-Development/JQUERY/index.js
@@ -1,1 +1,22 @@
-$("h1").css("color", "white");
+// $(document).ready(() => {
+//   $("h1").css("color", "black");
+// });
+
+// Normally selecting an element
+document.querySelector("h1");
+
+// Using JQUERY => to add classes
+$("h1").addClass("big-title");
+
+// No difference between selecting all buttons compared to one h1
+$("button");
+
+// How to check if an element has a class
+$("h1").hasClass("big-title");
+
+// Modify text of an element
+$("h1").text("bye");
+
+// Example of changing all of our buttons using the same method
+
+$("button").text("Don't Click Me");
