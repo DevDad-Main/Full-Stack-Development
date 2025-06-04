94dafc6166ff4a8c58398a3bb8d6c41d7c1f642b
Oliver <softwaredevdad@gmail.com>
2025-06-04 21:32:38 +0200

    Travel Tracker Error Handling

--- Backend/PostgreSQL/Travel-Tracker/index.js
+++ Backend/PostgreSQL/Travel-Tracker/index.js
@@ -4,10 +4,6 @@ import pg from "pg";
 
 const app = express();
 const port = 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
@@ -18,70 +14,87 @@ const db = new pg.Client({
 
 db.connect();
 
-let countries = [];
-let visitedCountries = [];
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-//TODO: Move this query to the post route so we query our visited countries and enter the data into the database
-//BUG: Due to use db.end() below in this query, our other query in the post route returns an error
-db.query("SELECT * FROM countries", (err, res) => {
-  if (err) {
-    console.log("Error exectuing query:", err.stack);
-  } else {
-    res.rows.forEach((country) => {
-      var data = {
-        Country: country.country_name.toLowerCase(),
-        Code: country.country_code,
-      };
-      countries.push(data);
-    });
-    // console.log(countries);
-  }
+let error;
 
-  db.end();
-});
+// //TODO: Move this query to the post route so we query our visited countries and enter the data into the database
+// //BUG: Due to use db.end() below in this query, our other query in the post route returns an error
+// db.query("SELECT * FROM countries", (err, res) => {
+//   if (err) {
+//     console.log("Error exectuing query:", err.stack);
+//   } else {
+//     res.rows.forEach((country) => {
+//       var data = {
+//         Country: country.country_name.toLowerCase(),
+//         Code: country.country_code,
+//       };
+//       countries.push(data);
+//     });
+//     // console.log(countries);
+//   }
+//   //FIX: Commenting this out due to closing the database here
+//   // our query below in the post route couldn't get executed because of this
+//   // db.end();
+// });
+//
+async function checkVisited() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+
+  let countries = [];
+
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
 
 app.get("/", async (req, res) => {
+  const countries = await checkVisited();
   res.render("index.ejs", {
-    countries: visitedCountries,
-    total: visitedCountries.length,
+    countries: countries,
+    total: countries.length,
+    error: error,
   });
 });
 
 app.post("/add", async (req, res) => {
-  const userInput = req.body.country.toLowerCase();
-  let resultCountryCode = "";
+  const userInput = req.body.country;
 
-  countries.forEach((country) => {
-    if (userInput === country.Country) {
-      if (visitedCountries.includes(country.Code)) {
-        console.log("Element Already Exists");
-        return;
-      } else {
-        console.log("Adding:", country.Code);
-      }
+  try {
+    const result = await db.query(
+      "SELECT country_code from countries WHERE country_name=$1",
+      [userInput],
+    );
 
-      resultCountryCode = country.Code;
-      visitedCountries.push(country.Code);
+    const data = result.rows[0];
+    const countryCode = data.country_code;
 
-      return;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode],
+      );
+      res.redirect("/");
+    } catch (err) {
+      console.log(err);
+      const countries = await checkVisited();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again",
+      });
     }
-  });
-
-  res.redirect("/");
-  // res.render("index.ejs", {
-  //   countries: visitedCountries,
-  //   total: visitedCountries.length,
-  // });
-  //
-  //BUG: Due to use db.end() below in this query, our other query in the post route returns an error
-  // const result = await db.query(
-  //   `INSERT INTO visited_countries
-  //   (country_code)
-  //   VALUES ($1)`,
-  //   [resultCountryCode],
-  // );
-  //
-  // console.log(result);
+  } catch (err) {
+    console.log(err);
+    const countries = await checkVisited();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again",
+    });
+  }
 });
 
 app.listen(port, () => {
